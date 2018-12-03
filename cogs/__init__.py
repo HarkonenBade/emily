67ba4bd5ac970e5543d9656c9a82767d0b97e94b
HarkonenBade <github@harkonen.net>
2018-12-03 23:37:26 +0000

    Port quote code

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,6 +1,7 @@
-from . import (management, misc, self_management)
+from . import (quote, management, misc, self_management)
 
 def setup(bot):
+    bot.add_cog(quote.Quote())
     bot.add_cog(management.Manage())
     bot.add_cog(misc.Misc())
     bot.add_cog(self_management.selfmanagement())
--- emily/run.py
+++ emily/run.py
@@ -1,10 +1,22 @@
 import os
 
+import pytumblr
+
 from . import extensions
 from .bot import Emily
 
 def main():
     client = Emily()
     client.load_extension("cogs")
+
+    if 'EMILY_DEBUG' in os.environ:
+        client.tumblr = None  # FIXME
+    else:
+        client.tumblr = pytumblr.TumblrRestClient(
+            os.environ['TUMBLR_CONSUMER_KEY'],
+            os.environ['TUMBLR_CONSUMER_SECRET'],
+            os.environ['TUMBLR_OAUTH_TOKEN'],
+            os.environ['TUMBLR_OAUTH_SECRET']
+        )
 
     client.run(os.environ['DISCORD_TOKEN'])
